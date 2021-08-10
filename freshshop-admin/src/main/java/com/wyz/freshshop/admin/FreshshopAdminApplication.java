13ec06eefb5af0cacd7163a1e624745ec6787c95
wangyouzheng <353735348@qq.com>
2021-08-10 15:37:20 +0800

    切换到nacos yml ApplicationsMain

--- freshshop-admin/src/main/java/com/wyz/freshshop/admin/FreshshopAdminApplication.java
+++ freshshop-admin/src/main/java/com/wyz/freshshop/admin/FreshshopAdminApplication.java
@@ -3,12 +3,12 @@ package com.wyz.freshshop.admin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
-// @EnableDiscoveryClient  nacos、zookeeper、consul服务发现客户端 springCloud生态中的注解
-@EnableEurekaClient
+@EnableDiscoveryClient  // nacos、zookeeper、consul服务发现客户端 springCloud生态中的注解
+// @EnableEurekaClient
 @EnableFeignClients
 @EnableCircuitBreaker // 开启断路/熔断 cloud common
 @SpringBootApplication
--- gateway/src/main/java/com/wyz/freshshop/gateway/GateWayApplication.java
+++ gateway/src/main/java/com/wyz/freshshop/gateway/GateWayApplication.java
@@ -2,10 +2,10 @@ package com.wyz.freshshop.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-// @EnableDiscoveryClient // 开启服务发现
-@EnableEurekaClient
+@EnableDiscoveryClient // 开启服务发现
+// @EnableEurekaClient
 @SpringBootApplication
 public class GateWayApplication {
 
